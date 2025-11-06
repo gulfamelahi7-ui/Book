e755f12d33ebf53c7b7a38464c2c4870b160e9d5
Rana gulfam <gulfamelahi7@gmail.com>
2025-11-06 20:30:29 +0500

    heading tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Gulfam from './components/Gulfam';
+import Seven from './components/seven';
 
 function App() {
   return (
     <div >
       <Gulfam/>
+      <Seven/>
     </div>
   );
 }
